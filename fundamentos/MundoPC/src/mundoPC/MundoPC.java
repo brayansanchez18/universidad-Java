a95ddbbb5f85a38b0d0b08e62752b02739739782
Emanuel <brayan.sanchez.contacto@gmail.com>
2023-02-20 21:20:55 -0600

    terminamos
    se termina la sección de fundamentos y se inicia con la sección de programación

---
+++ fundamentos/MundoPC/src/mundoPC/MundoPC.java
@@ -0,0 +1,22 @@
+package mundoPC;
+
+import mx.com.es.mundopc.*;
+
+public class MundoPC {
+    public static void main(String[] args) {
+        Monitor monitorHP = new Monitor("HP", 13);       
+        Teclado tecladoHP = new Teclado("bluetooth", "HP");
+        Raton ratonHP = new Raton("bluetooth", "HP");
+        Computadora computadoraHP = new Computadora("Computadora HP", monitorHP, tecladoHP, ratonHP);
+        
+        Monitor monitorGamer = new Monitor("Gamer", 42);       
+        Teclado tecladoGamer = new Teclado("bluetooth", "Gamer");
+        Raton ratonGamer = new Raton("bluetooth", "Gamer");
+        Computadora computadoraGamer = new Computadora("Computadora Gamer", monitorGamer, tecladoGamer, ratonGamer);
+        
+        Orden orden1 = new Orden();
+        orden1.agregarComputadora(computadoraHP);
+        orden1.agregarComputadora(computadoraGamer);
+        orden1.mostrarOrden();
+    }
+}
